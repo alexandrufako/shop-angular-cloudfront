68c93df470cc9776a9609c419774e71e92445925
Alexandru Fako <alexandru.fako@gmail.com>
2025-10-05 22:24:57 +0300

    forgot some files

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
 import { switchMap } from 'rxjs/operators';
+import { ApiEndpoint } from 'src/environments/config.interface';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
   uploadProductsCSV(file: File): Observable<unknown> {
-    if (!this.endpointEnabled('import')) {
+    if (!this.endpointEnabled('import' as ApiEndpoint)) {
       console.warn(
         'Endpoint "import" is disabled. To enable change your environment.ts config',
       );
@@ -26,7 +27,7 @@ export class ManageProductsService extends ApiService {
   }
 
   private getPreSignedUrl(fileName: string): Observable<string> {
-    const url = this.getUrl('import', 'import');
+    const url = this.getUrl('import' as ApiEndpoint, 'import');
 
     return this.http.get<string>(url, {
       params: {
